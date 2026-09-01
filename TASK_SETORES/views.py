2a9d4091bcd7dc41d97122f366e536e669825f57
teylon <oteylon84@gmail.com>
2026-09-01 01:35:49 -0300

    .

--- TASK_SETORES/views.py
+++ TASK_SETORES/views.py
@@ -1,7 +1,6 @@
 from django.contrib import messages
-from django.shortcuts import render
-from .models import Setor, Task, Membro,Daily_Meeting,Daily_Note
 from django.shortcuts import render, redirect, get_object_or_404
+from .models import Setor, Task, Membro, Daily_Meeting, Daily_Note
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
@@ -9,43 +8,50 @@ def tela_login(request):
     if request.method == 'POST':
         usuario = request.POST.get('username')
         senha = request.POST.get('password')
-        usuario_banco = authenticate(request, username = usuario, password = senha)
+        usuario_banco = authenticate(request, username=usuario, password=senha)
         if usuario_banco is not None:
-            login (request, usuario_banco)
-            return redirect ('')
+            login(request, usuario_banco)
+            return redirect('dashboard') 
         else:
-            messages.error(request,'Membro não encontrado ou senha incorreta')
-            return redirect('')
-    return render(request, '')
+            messages.error(request, 'Membro não encontrado ou senha incorreta')
+            return redirect('login') 
+    return render(request, 'login.html') 
         
 @login_required(login_url='login')
 def dashboard(request):
     meu_setor = request.user.setor
-    tarefa_do_setor = Task.objects.filter(setor = meu_setor)
+    tarefa_do_setor = Task.objects.filter(setor=meu_setor)
     context = {
         'tarefas': tarefa_do_setor,
         'setor_atual': meu_setor
     }
-    return render(request, '', context)
+    return render(request, 'dashboard.html', context)
 
 @login_required(login_url='login')
 def cadastro_tarefas(request):
     if request.user.cargo not in ['senior', 'chefe']:
         messages.error(request, "Você não tem permissão para criar tarefas.")
         return redirect('dashboard')
+        
     if request.method == 'POST':
-        titulo = request.POST.get('Titulo')
-        descricao = request.POST.get('Descrição')
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
         prioridade = request.POST.get('prioridade')
+        data_entrega_digitada = request.POST.get('data_entrega') 
+        
         id_do_membro = request.POST.get('membro_atribuido')
         membro_recebedor = Membro.objects.get(id=id_do_membro)
-        data_entrega_digitada = request.POST.get('data_entrega') 
+        
         Task.objects.create(
             titulo=titulo,
             descricao=descricao,
             prioridade=prioridade,
             data_entrega=data_entrega_digitada, 
+            setor=request.user.setor,
+            responsavel=request.user,
+            membro=membro_recebedor
         )
         messages.success(request, 'Tarefa criada com sucesso!')
         return redirect('dashboard')
-    
+    equipe_do_setor = Membro.objects.filter(setor=request.user.setor)
+    return render(request, 'cadastro_tarefa.html', {'equipe': equipe_do_setor})
