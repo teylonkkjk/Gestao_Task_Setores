419523f918d5c3e8c8d92d91d1db36b65c7f531d
teylon <oteylon84@gmail.com>
2026-09-01 22:26:02 -0300

    finalizado

--- TASK_SETORES/urls.py
+++ TASK_SETORES/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.tela_login, name='login'),
+    path('', views.tela_login, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('cadastro-tarefa/', views.cadastro_tarefas, name='criar_tarefa'),
     path('mudar-status/<int:task_id>/', views.mudar_status_tarefa, name='mudar_status'),
